d2a0e0238e568b8ee71dbc027449f1e29102e74d
Juan Roldan <juanroldan1989@gmail.com>
2016-05-07 03:38:23 +1200

    MyShips list added into game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,10 +3,16 @@ class GamesController < ApplicationController
   before_action :set_opponent
   before_action :set_user
 
+  helper_method :my_ships
+
   def show
   end
 
   private
+
+  def my_ships
+    @user.ships
+  end
 
   def set_opponent
     case params[:id]
