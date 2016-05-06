53d3d1bfb5764e3c6b754e9c6aacb2addc1323c9
Juan Roldan <juanroldan1989@gmail.com>
2016-05-07 03:39:00 +1200

    Seeds updated - Ships created in different positions for both players

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,78 @@ User.create(name: "Player 1")
 User.create(name: "Player 2")
 
 puts "Done !"
+
+puts
+puts "Setting up Ships..."
+
+Ship.create(
+  user_id:  1,
+  title:    "Aircraft carrier",
+  location: ["a1", "a2", "a3", "a4", "a5"],
+  size:     5
+)
+
+Ship.create(
+  user_id:  1,
+  title:    "Battleship",
+  location: ["b1", "b2", "b3", "b4"],
+  size:     4
+)
+
+Ship.create(
+  user_id:  1,
+  title:    "Cruiser",
+  location: ["c1", "c2", "c3"],
+  size:     3
+)
+
+Ship.create(
+  user_id:  1,
+  title:    "Destroyer",
+  location: ["d1", "d2"],
+  size:     2
+)
+
+Ship.create(
+  user_id:  1,
+  title:    "Submarine",
+  location: ["e1"],
+  size:     1
+)
+
+Ship.create(
+  user_id:  2,
+  title:    "Aircraft carrier",
+  location: ["j1", "j2", "j3", "j4", "j5"],
+  size:     5
+)
+
+Ship.create(
+  user_id:  2,
+  title:    "Battleship",
+  location: ["i7", "i8", "i9", "i10"],
+  size:     4
+)
+
+Ship.create(
+  user_id:  2,
+  title:    "Cruiser",
+  location: ["h6", "h7", "h8"],
+  size:     3
+)
+
+Ship.create(
+  user_id:  2,
+  title:    "Destroyer",
+  location: ["f10", "g10"],
+  size:     2
+)
+
+Ship.create(
+  user_id:  2,
+  title:    "Submarine",
+  location: ["a10"],
+  size:     1
+)
+
+puts "Done!"
